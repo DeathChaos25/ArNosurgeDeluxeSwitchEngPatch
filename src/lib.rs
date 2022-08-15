a8d88692faac52443ead3f260dcfb1d733eb0b1f
Raytwo <raytwost@gmail.com>
2022-08-16 00:53:22 +0200

    Add temporary hook for character biography menu

--- src/lib.rs
+++ src/lib.rs
@@ -40,11 +40,25 @@ pub struct MenuNameEntry {
     pub unk5: u32,
 }
 
+/// Don't use this for now
 #[skyline::hook(offset = 0x19dd30, inline)]
 pub fn battle_name_hook(ctx: &mut InlineCtx) {
-    // Don't use this for now
     let names : [*const u8;3] = [b"Cass\0".as_ptr() , b"Cass\0".as_ptr(), b"Cass\0".as_ptr()];
     unsafe { *ctx.registers[8].x.as_mut() = names.as_ptr() as u64; }
+}
+
+
+#[skyline::hook(offset = 0x23dbb0, inline)]
+pub fn encyclopedia_characters_met_left_title_string_hook(ctx: &mut InlineCtx) {
+    unsafe { *ctx.registers[1].x.as_mut() = skyline::c_str("Characters\0") as u64; }
+    println!("Character");
+}
+
+// TODO: Move the bio stuff in a /ui/menus/bio.rs module at this point
+#[skyline::hook(offset = 0x23c3a0, inline)]
+pub fn chara_bio_description_hook(ctx: &mut InlineCtx) {
+    // Character length needs fixing through the layout file.
+    unsafe { *ctx.registers[20].x.as_mut() = b"Reach enlightenment. Seek the thighs. Avoid the socks.\0\0".as_ptr() as u64; }
 }
 
 #[skyline::main(name = "nosurge")]
@@ -58,7 +72,7 @@ pub fn main() {
     menu_name_entries[7].name = skyline::c_str("Delta\0");
 
     ui::install_hook();
-    //skyline::install_hook!(name_hook);
+    skyline::install_hooks!(encyclopedia_characters_met_left_title_string_hook, chara_bio_description_hook);
 
     println!("Ar noSurge English Patch is now installed");
 }
