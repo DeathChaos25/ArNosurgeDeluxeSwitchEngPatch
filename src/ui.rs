b2635d5cc29268291e04afee37cddc0beb07e622
DeathChaos <deathchaos25@gmail.com>
2022-08-15 17:22:38 -0400

    cleanup, replace patch data with nop_patches

--- src/ui.rs
+++ src/ui.rs
@@ -1,4 +1,4 @@
-use skyline::{hooks::InlineCtx, patching::patch_data};
+use skyline::{hooks::InlineCtx, patching::{patch_data, nop_data, patch_str}};
 
 const LOG_UITEXT: &[u8] = &[ 0xef, 0xbc, 0xac, 0xef, 0xbd, 0x8f, 0xef, 0xbd, 0x87, 0x0d, 0x0a, 0x00, 0x00]; // Log, but wide
 const SEND_UITEXT: &[u8] = &[ 0xef, 0xbc, 0xae, 0xef, 0xbd, 0x85, 0xef, 0xbd, 0x98, 0xef, 0xbd, 0x94, 0x00, 0x00]; // Next, but wide
@@ -28,13 +28,25 @@ fn ui_text_send2_hook(ctx: &mut InlineCtx) {
     unsafe { *ctx.registers[20].x.as_mut() = SEND_UITEXT.as_ptr() as u64; }
 }
 
-pub fn install_hook() {
-    // NOP some UI-related instructions
+fn nop_patches() { // NOP some UI-related instructions
     unsafe {
-        const NOP: u32 = 0xD503201F;
-        patch_data(0x001335c4, &NOP).expect("Unable to patch 0x001335c4");
-        patch_data(0x0013369c, &NOP).expect("Unable to patch 0x0013369c");
+        nop_data(0x001335c4);
+        nop_data(0x0013369c);
+    }
+}
+
+fn overwrite_names() { // Names that can be patched in place
+    unsafe {
+        patch_str(0x006151d9, "Cass").expect("Unable to patch 0x006151d9");
+        patch_str(0x0067399b, "Ion").expect("Unable to patch 0x0067399b");
+        patch_str(0x006151cf, "Delta").expect("Unable to patch 0x006151cf");
     } 
+}
+
+pub fn install_hook() {
+
+    nop_patches();
+    overwrite_names();
 
     skyline::install_hooks!(char_limit_textbox_hook, char_limit_log_hook);
     skyline::install_hook!(ui_text_log_hook);
