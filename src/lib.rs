737deb3c0e99214394a507de36a8f41d8543c34b
Raytwo <raytwost@gmail.com>
2022-08-18 20:23:56 +0200

    Fix offsets for 1.0.1

--- src/lib.rs
+++ src/lib.rs
@@ -54,7 +54,7 @@ pub fn get_battle_name_by_id(idx: u32) -> *const u8 {
 }
 
 /// Don't use this for now
-#[skyline::hook(offset = 0x19de04, inline)]
+#[skyline::hook(offset = 0x19de00, inline)]
 pub fn battle_name_hook(ctx: &mut InlineCtx) {
     let names = [
         b"Cass\0".as_ptr() , 
@@ -349,7 +349,7 @@ pub fn main() {
     // Menu name patches
     let text_region = unsafe { skyline::hooks::getRegionAddress(skyline::hooks::Region::Text) as *const u8 };
     // TODO: Un-hardcode the offset? Except if there is no update for the game.
-    let menu_name_entries = unsafe { std::slice::from_raw_parts_mut(text_region.add(0xc94fc0) as *mut MenuNameEntry, 166) };
+    let menu_name_entries = unsafe { std::slice::from_raw_parts_mut(text_region.add(0xc98fc0) as *mut MenuNameEntry, 166) };
     menu_name_entries[5].name = skyline::c_str("Cass\0");
     menu_name_entries[6].name = skyline::c_str("Ion\0");
     menu_name_entries[7].name = skyline::c_str("Delta\0");
